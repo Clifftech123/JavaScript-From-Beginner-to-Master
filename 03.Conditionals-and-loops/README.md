6cef1dbab92311449b7e5007e92f86259d9bb286
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-06-07 08:27:49 +0000

    Adding of new code

---
+++ 03.Conditionals-and-loops/README.md
@@ -0,0 +1,12 @@
+ ## Part 3 : Conditionals and loops
+
+
+Welcome to the third part of the JavaScript from Beginner to Master Tutorial Series.
+
+The topics covered in this section include:
+
+- Conditionals
+- Loops
+ You all the code in the app.js file in this folder.
+
+Thank you for reading! You can find the full article [here](https://dev.to/cliff123tech/conditionals-and-loops-in-javascript-3h0m).
--- 03.Conditionals-and-loops/app.js
+++ 03.Conditionals-and-loops/app.js
@@ -1,12 +1,10 @@
-//  Welcome the conditional and loops section all the code is here . 
+//  Welcome the conditional and loops section all the code is here .
 
 //  cod  statement syntax
 
 // if (condition) {
 //     //  code to execute if condition is true
 // }
-
-
 
 //  if  statement example
 let age = 18;
@@ -15,7 +13,6 @@ if (age >= 18) {
 } else {
   console.log("You are not old enough to vote yet.");
 }
-
 
 //  if else if  statement example
 
@@ -28,17 +25,14 @@ if (temperature > 30) {
   console.log("The temperature is just right.");
 }
 
-
 // last  example of if else if statement
 
 let isRaining = true;
 if (isRaining) {
-    console.log("Remember to bring an umbrella.")
-    
+  console.log("Remember to bring an umbrella.");
 }
 
-
-//   Switch statement example
+//   Switch statement example 1
 let day = "Monday";
 switch (day) {
   case "Monday":
@@ -51,5 +45,47 @@ switch (day) {
     console.log("Today is Wednesday.");
     break;
   default:
-        console.log("Today is some other day.");
+    console.log("Today is some other day.");
 }
+
+// Switch statement example 2
+let fruit = "apple";
+switch (fruit) {
+  case "banana":
+  case "apple":
+    console.log("The fruit is either a banana or an apple.");
+    break;
+  case "orange":
+    console.log("The fruit is an orange.");
+    break;
+  default:
+    console.log("The fruit is unknown.");
+}
+
+// ==  Loops  ==
+
+//  For loop example
+for (let i = 0; i < 5; i++) {
+  console.log(i); // 0, 1, 2, 3, 4
+}
+
+// do while loop example
+
+let i = 0;
+
+while (i < 5) {
+  console.log(i); // 0, 1, 2, 3, 4
+  i++;
+}
+
+//  while loop example
+let TK = 0;
+
+do {
+  console.log(TK); // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+  TK++;
+} while (TK < 10);
+
+
+
+// THANK YOU FOR READING THIS CODE
